c33657862073522efd3cc189a44fe4dd3256be14
profsibz3090 <sibandadonty@gmail.com>
2025-01-07 14:21:03 +0200

    contact us form added

--- src/components/contact-us/ContactUsHero.tsx
+++ src/components/contact-us/ContactUsHero.tsx
@@ -1,5 +1,3 @@
-import ProfileRings from "./ProfileRings"
-
 
 const ContactUsHero = () => {
   return (
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,7 @@
 import ballImage from "../assets/CODAXLABLOGO.png";
 import { useState } from "react";
 import ContactUsHero from "../components/contact-us/ContactUsHero";
+import ContactUsForm from "../components/contact-us/ContactUsForm";
 
 const faqsQ: {
   id: string;
@@ -62,57 +63,7 @@ export default function Contact() {
       {/* Main Content */}
       <div className="grid gap-8 lg:grid-cols-2">
         {/* Contact Form */}
-        <div className="space-y-6">
-          <h1 className="text-center mb-4 text-4xl font-semibold">
-            We'd Love to Hear From Your!
-          </h1>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <label className="text-left" htmlFor="name">
-                Full Name
-              </label>
-              <input
-                id="name"
-                placeholder="Enter your name"
-                className="bg-gray-900 p-2 rounded-md"
-              />
-            </div>
-            <div className="grid gap-2">
-              <label className="text-left" htmlFor="email">
-                Email
-              </label>
-              <input
-                id="email"
-                type="email"
-                placeholder="Enter your email"
-                className="bg-gray-900 p-2 rounded-md"
-              />
-            </div>
-            <div className="grid gap-2">
-              <label className="text-left" htmlFor="subject">
-                Subject
-              </label>
-              <input
-                id="subject"
-                placeholder="Enter subject"
-                className="bg-gray-900 p-2 rounded-md"
-              />
-            </div>
-            <div className="grid gap-2">
-              <label className="text-left" htmlFor="message">
-                Message
-              </label>
-              <textarea
-                id="message"
-                placeholder="Enter your message"
-                className="min-h-[150px] bg-gray-900 p-2 rounded-md"
-              />
-            </div>
-          </div>
-          <button className="w-full bg-blue-600 hover:bg-blue-700 p-3 rounded-md">
-            Send Message
-          </button>
-        </div>
+        <ContactUsForm />
 
         {/* Contact Information */}
         <div className="space-y-8 bg-white rounded-xl shadow-lg py-4 px-5">
