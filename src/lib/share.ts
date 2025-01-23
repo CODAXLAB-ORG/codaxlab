87e2e96356950dd8a81fddd6310fa9d8721b6110
p34-pac <peterpaultolulope@gmail.com>
2025-01-23 16:48:30 +0100

    fix: enhance mobile detection and improve share functionality

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -6,7 +6,7 @@
  * @param {string} [options.title] - The title of the content (optional).
  * @param {string} [options.text] - A description or message to include with the shared content (optional).
  * @param {string} [options.url] - The URL to share (optional). If fallback is used, this will be copied to the clipboard.
- * 
+ *
  * @example
  * // Example usage:
  * shareContent({
@@ -16,40 +16,37 @@
  * });
  */
 export function shareContent({
-    title,
-    text,
-    url,
-  }: {
-    title?: string;
-    text?: string;
-    url?: string;
-  }): void {
-    if (window.navigator.share) {
-      // Use Web Share API if supported
-      alert("sharing")
-      window.navigator
-        .share({ title, text, url })
-        .then(() => console.log('Content shared successfully!'))
-        .catch((error) => {
-          console.error('Error sharing content:', error);
-          alert('Sharing failed. Please try again.');
+  title,
+  text,
+  url,
+}: {
+  title?: string;
+  text?: string;
+  url?: string;
+}): void {
+  if (window.navigator.share) {
+    // Use Web Share API if supported
+    window.navigator
+      .share({ title, text, url })
+      .then(() => {
+        return;
+      })
+      .catch(() => {
+        return;
+      });
+  } else {
+    // Fallback: Copy URL to clipboard
+    if (url) {
+      window.navigator.clipboard
+        .writeText(url)
+        .then(() => {
+          return;
+        })
+        .catch(() => {
+          return;
         });
     } else {
-      // Fallback: Copy URL to clipboard
-      if (url) {
-        window.navigator.clipboard
-          .writeText(url)
-          .then(() => {
-            alert('Link copied to clipboard!');
-            console.log('URL copied to clipboard:', url);
-          })
-          .catch((error) => {
-            console.error('Error copying to clipboard:', error);
-            alert('Failed to copy the link to the clipboard.');
-          });
-      } else {
-        alert('Sharing is not supported, and no URL was provided.');
-      }
+      return;
     }
   }
-  
+}
